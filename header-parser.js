7665016a76cbed0e4b1a7062728792b8e41c9a86
Eremin <mitya1705@gmail.com>
2017-06-08 08:56:42 +0000

    port logic

--- header-parser.js
+++ header-parser.js
@@ -1,6 +1,7 @@
 const config = require('config')
 const express = require('express')
 const app = express()
+var port = process.env.PORT || 5000;
 
 app.get('/', function(req, res){
     var requestInfo = {
@@ -12,6 +13,6 @@ app.get('/', function(req, res){
     res.send(requestInfo); 
 }) 
 
-app.listen(config.port, function () {
-  console.log('Example app listening on port ' + config.port)
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port)
 })
